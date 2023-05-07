4fff42f489a219b7dd8da38933ba1dbefcaffa49
yeonwoo shin <tlsxkrqhd@naver.com>
2023-05-08 02:10:07 +0900

    menuPageMaking

--- src/PageRouter.tsx
+++ src/PageRouter.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
 
 export const PageRouter = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const PageRouter = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "menu",
+        element: <Menu />,
       },
     ],
   },
